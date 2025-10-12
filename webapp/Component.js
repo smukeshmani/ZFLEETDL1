356e0dcc383b60e50c6c96abfca1545bf96dea6f
Webide Developer <it@berain.com.sa>
2025-10-12 12:57:35 +0000

    12102025

---
+++ webapp/Component.js
@@ -0,0 +1,11 @@
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/Device", "ZFLEET_DELIVERY_TRUCK_REQ/ZFLEET_DELIVERY_TRUCK_REQ/model/models"], 
+    (e, t, i) => {
+        "use strict"; return e.extend("ZFLEET_DELIVERY_TRUCK_REQ.ZFLEET_DELIVERY_TRUCK_REQ.Component", {
+            metadata: { manifest: "json" },
+            init: function () {
+                e.prototype.init.apply(this, arguments);
+                this.getRouter().initialize();
+                this.setModel(i.createDeviceModel(), "device");
+            }
+        });
+    });
